cb187c3f5b9553098a177d9f5d53501edfa8d091
¨Deborah <deborah.oliveiraa@outlook.com.br>
2023-02-14 12:38:40 -0300

    style: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import './global.css'
+import styles from './App.module.css'
+import { Header } from "./components/Header"
+import { NewTask } from './components/NewTask'
+import { TaskList } from './components/TaskList'
+
 function App() {
 
   return (
-    <div className="App">
+    <div>
+      <Header/>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <NewTask/>
+          <TaskList/>
+        </main>
+      </div>
     </div>
   )
 }
